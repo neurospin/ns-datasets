d734c06be18099d14e7c71727829f8ff625ce857
duchesnay <edouard.duchesnay@gmail.com>
2021-02-09 16:19:53 +0100

    FIX: utils.py, to be Fixed bu Julie

--- localizer/cat12vbm_make_dataset.py
+++ localizer/cat12vbm_make_dataset.py
@@ -3,7 +3,7 @@ from utils import cat12_nii2npy
 ## Dataset name for the generated output .npy and .csv files
 dataset = 'localizer'
 ## Initial .nii files
-nii_regex_path = '/neurospin/psy_sbox/'+dataset+'/derivatives/cat12-12.6_vbm/sub-*/ses-*/anat/mri/mwp1*.nii'
+nii_regex_path = '/neurospin/psy/'+dataset+'/derivatives/cat12-12.6_vbm/sub-*/ses-*/anat/mri/mwp1*.nii'
 ## Phenotype obtained with <phenotypes_make_dataset.py>
 phenotype_path = '/neurospin/psy_sbox/{dataset}/{dataset_u}_t1mri_mwp1_participants.csv'.format(dataset=dataset,
                                                                                                 dataset_u=dataset.upper())
@@ -18,3 +18,18 @@ id_type = str
 
 cat12_nii2npy(nii_regex_path, phenotype_path, dataset, output_path,
             qc=qc_path, sep=sep, id_type=id_type, check=dict(shape=(121, 145, 121),zooms=(1.5, 1.5, 1.5)))
+
+"""
+nii_path = nii_regex_path
+dataset_name = dataset
+output_path
+qc=qc_path
+id_type=id_type
+check=dict(shape=(121, 145, 121),zooms=(1.5, 1.5, 1.5))
+
+import nibabel
+from nitk.image import img_to_array, global_scaling, compute_brain_mask, rm_small_clusters, img_plot_glass_brain
+from nitk.bids import get_keys
+from nitk.data import fetch_data
+
+"""
